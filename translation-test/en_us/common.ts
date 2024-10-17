dc2a4539c6b5ca462c5233d522b899a5b9240493
Mayuko Sawai <mayuko.sawai@smarthr.co.jp>
2024-10-17 17:30:49 +0900

    File common.ts committed.

--- translation-test/en_us/common.ts
+++ translation-test/en_us/common.ts
@@ -1,6 +1,6 @@
 export const locale = {
   // 共通文言
-  '_Shared/TestWord1': '',
-  '_Shared/TestWord2': '',
-  '_Shared/Testzword3': '',
+  '_Shared/TestWord1': 'Human Resources and Labor',
+  '_Shared/TestWord2': 'Application',
+  '_Shared/Testzword3': 'Procedure',
   } as const
