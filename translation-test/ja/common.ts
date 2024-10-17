04d59af2dd6d5cc6d39210bf3d6ed9001e4ee703
Mayuko Sawai <mayuko.sawai@smarthr.co.jp>
2024-10-17 13:50:28 +0900

    Update common.ts

--- translation-test/ja/common.ts
+++ translation-test/ja/common.ts
@@ -1,6 +1,6 @@
 export const locale = {
   // 共通文言
-  '_Shared/CommonPageTitle': '人事労務',
-  '_Shared/Form/Required': '申請',
-  '_Shared/Form/Optional': '手続き',
+  '_Shared/TestWord1': '人事労務',
+  '_Shared/TestWord2': '申請',
+  '_Shared/Testzword3': '手続き',
   } as const
